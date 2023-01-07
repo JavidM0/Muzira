e1dbed17b57abfc6857dfe65edb718a8373fbeba
JavidM0 <muradov.cavid02@gmail.com>
2023-01-07 18:40:53 +0300

    add presentation layer

--- app/src/main/java/com/example/muzira/di/AppModule.kt
+++ app/src/main/java/com/example/muzira/di/AppModule.kt
@@ -1,11 +1,9 @@
 package com.example.muzira.di
 
-import org.koin.core.module.Module
 import org.koin.dsl.module
 
 val appModule = module {
-
-    val appModule: List<Module> = dataModule +
+    dataModule +
             domainModule +
             presentationModule
 }
